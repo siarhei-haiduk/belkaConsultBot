e35aa6d0ecf9fb0ecd870fa6864fe2992610f875
siarhei-haiduk <yakamoto2008@gmail.com>
2025-02-21 17:01:35 +0400

    Second commit

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,16 +1,28 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Чат')],
-    [KeyboardButton(text='Генерация картинок')]
-],
-    resize_keyboard=True,
-    input_field_placeholder='Выберите пункт меню.'
-)
+from app.database.requests import get_user
 
 
-cancel = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Отмена')]
-],
-    resize_keyboard=True
-)
+async def main(tg_id):
+    kb = ReplyKeyboardBuilder()
+    user = await get_user(tg_id)
+    if not user.consulted:
+        kb.button(text='Консультация')
+    if not user.portfolio:
+        kb.button(text='Портфолио')
+
+    return kb.as_markup(resize_keyboard=True, remove_keyboard=True)
+
+
+consult = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text='Бесплатная консультация')]],  resize_keyboard=True)
+
+
+async def contact(tg_id):
+    kb = ReplyKeyboardBuilder()
+    user = await get_user(tg_id)
+    if not user.contact_sent:
+        kb.button(text='Отправить свой контакт', request_contact=True)
+
+    return kb.as_markup(resize_keyboard=True, remove_keyboard=True)
